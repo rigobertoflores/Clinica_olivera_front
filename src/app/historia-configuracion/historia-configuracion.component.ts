f1ecbc4b7b4cf313bdf66022c9211eabd3541fa4
rigobertoflores <93140192+rigobertoflores@users.noreply.github.com>
2024-06-13 13:56:26 -0500

    ajustes para mover cursor en texAerea, y cambio de orden en historias

--- src/app/historia-configuracion/historia-configuracion.component.ts
+++ src/app/historia-configuracion/historia-configuracion.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormsModule } from '@angular/forms';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
@@ -8,87 +8,181 @@ import { Historia } from '../interface/Historia';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Service } from './../Services/Service';
 import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
+import { LoadingComponent } from '../loading/loading.component';
 
 @Component({
   selector: 'app-historia-configuracion',
   standalone: true,
   templateUrl: './historia-configuracion.component.html',
   styleUrl: './historia-configuracion.component.css',
-  imports: [CKEditorModule, FormsModule, SidebarComponent, MenuComponent,ReactiveFormsModule,CommonModule]
+  imports: [
+    CKEditorModule,
+    FormsModule,
+    SidebarComponent,
+    MenuComponent,
+    ReactiveFormsModule,
+    CommonModule,
+    LoadingComponent,
+  ],
 })
-export class HistoriaConfiguracionComponent  implements OnInit{
-
+export class HistoriaConfiguracionComponent implements OnInit {
   listahistorias: Historia[];
   public Editor = ClassicEditor;
   fullScreenMode = false;
   @Input() data: string = '';
   historiaform: FormGroup;
   historias: Historia[];
-  historiaSeleccionada: number=0;
+  historiaSeleccionada: number = 0;
   his: Historia;
-  
+  showLoading: boolean = false;
+
   ajustarAltura(elemento: HTMLTextAreaElement): void {
     elemento.style.height = 'auto'; // Resetea la altura para calcular correctamente
     elemento.style.height = elemento.scrollHeight + 'px'; // Ajusta la altura al contenido
-}
+  }
 
-  constructor(private Service: Service){}
+  constructor(private Service: Service) {}
 
-  ngOnInit(): void {    
+  ngOnInit(): void {
     this.cargarListaHistoria();
     this.cargarFormulario(this.his);
   }
-  
 
   guardarEditarHistoria() {
+    this.showLoading = true;
     if (!this.historiaform.invalid) {
-      const historia : Historia={id:this.historiaform.get('id')?.value || 0,hc:this.historiaform?.get('hc')?.value,nombre:this.historiaform.get('nombre')?.value}
-      this.Service.postData('PostHistoria',historia).subscribe(
-        (result:Historia[])=>{
-          this.data="";
-         this.listahistorias=result;
-         this.historias=result;
-         this.historiaform.get('hc')?.setValue(''); // Contenido de la historia
-         this.historiaform.get('id')?.setValue(0); // ID de la historia
-         this.historiaform.get('nombre')?.setValue(''); // No
-         this.historiaSeleccionada=0;
+      const historia: Historia = {
+        id: this.historiaform.get('id')?.value || 0,
+        hc: this.historiaform?.get('hc')?.value,
+        nombre: this.historiaform.get('nombre')?.value,
+      };
+      this.Service.postData('PostHistoria', historia).subscribe(
+        (result: Historia[]) => {
+          this.data = '';
+          this.listahistorias = result;
+          this.historias = result;
+          this.historiaform.get('hc')?.setValue(''); // Contenido de la historia
+          this.historiaform.get('id')?.setValue(0); // ID de la historia
+          this.historiaform.get('nombre')?.setValue(''); // No
+          this.historiaSeleccionada = 0;
+          this.showLoading = false;
+          Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Se han guardado la historia correctamente',
+            showConfirmButton: false,
+            timer: 2000,
+          });
         }
-      )
+      );
     }
-  };
-
- 
-  cargarFormulario(his:Historia) {
-    this.historiaform = new FormGroup({
-      hc:new FormControl(),
-      id:new FormControl(),
-      nombre:new FormControl(),
-    })
   }
 
-  cargarListaHistoria(){
-    this.Service.getUnico('GetAllHistorias').subscribe(
-      (data: Historia[]) => {
-        if(data!=null){
-        this.historias=data;
-      }}
-    );
+  cargarFormulario(his: Historia) {
+    this.historiaform = new FormGroup({
+      hc: new FormControl(),
+      id: new FormControl(),
+      nombre: new FormControl(),
+    });
+  }
+
+  cargarListaHistoria() {
+    this.Service.getUnico('GetAllHistorias').subscribe((data: Historia[]) => {
+      if (data != null) {
+        this.historias = data;
+      }
+    });
   }
 
   onHistoriaChange($event: any) {
-    if(this.historiaSeleccionada==0){
+    if (this.historiaSeleccionada == 0) {
       this.historiaform.get('hc')?.setValue(''); // Contenido de la historia
       this.historiaform.get('id')?.setValue(0); // ID de la historia
       this.historiaform.get('nombre')?.setValue(''); // No
-    }else{
-    const histo = this.historias.find(historia => historia.id == this.historiaSeleccionada);
-    if (histo != null) {
-      this.historiaform.get('hc')?.setValue(histo.hc); // Contenido de la historia
-      this.historiaform.get('id')?.setValue(histo.id); // ID de la historia
-      this.historiaform.get('nombre')?.setValue(histo.nombre); // Nombre de la historia
-      setTimeout(() => this.ajustarAltura(document.getElementById('nombreinputtexthistoria') as HTMLTextAreaElement), 0);
+    } else {
+      const histo = this.historias.find(
+        (historia) => historia.id == this.historiaSeleccionada
+      );
+      if (histo != null) {
+        this.historiaform.get('hc')?.setValue(histo.hc); // Contenido de la historia
+        this.historiaform.get('id')?.setValue(histo.id); // ID de la historia
+        this.historiaform.get('nombre')?.setValue(histo.nombre); // Nombre de la historia
+        setTimeout(
+          () =>
+            this.ajustarAltura(
+              document.getElementById(
+                'nombreinputtexthistoria'
+              ) as HTMLTextAreaElement
+            ),
+          0
+        );
+      }
     }
   }
- }
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    const textarea = document.getElementById(
+      'nombreinputtexthistoria'
+    ) as HTMLTextAreaElement;
+    if (!textarea) return;
+
+    const cursorPosition = textarea.selectionStart;
+    const lines = textarea.value.substr(0, cursorPosition).split('\n');
+    const currentLineIndex = lines.length - 1;
+    const currentLine = lines[currentLineIndex];
+
+    if (event.key === 'ArrowUp') {
+      event.preventDefault();
+      this.moveCursorUp(textarea, currentLineIndex, cursorPosition, lines);
+    } else if (event.key === 'ArrowDown') {
+      event.preventDefault();
+      this.moveCursorDown(
+        textarea,
+        currentLineIndex,
+        cursorPosition,
+        lines,
+        currentLine.length
+      );
+    }
+  }
+
+  private moveCursorUp(
+    textarea: HTMLTextAreaElement,
+    currentLineIndex: number,
+    cursorPosition: number,
+    lines: string[]
+  ) {
+    if (currentLineIndex === 0) return; // Ya está en la primera línea
+
+    const previousLineLength = lines[currentLineIndex - 1].length;
+    const newCursorPosition =
+      cursorPosition - lines[currentLineIndex].length - 1;
+
+    // Asegurar que la nueva posición no sea negativa
+    const adjustedPosition = newCursorPosition >= 0 ? newCursorPosition : 0;
+    textarea.setSelectionRange(adjustedPosition, adjustedPosition);
+  }
+
+  private moveCursorDown(
+    textarea: HTMLTextAreaElement,
+    currentLineIndex: number,
+    cursorPosition: number,
+    lines: string[],
+    currentLineLength: number
+  ) {
+    const nextLines = textarea.value.substr(cursorPosition).split('\n');
+    if (nextLines.length <= 1) return; // Ya está en la última línea
+
+    const newCursorPosition = cursorPosition + currentLineLength + 1;
+
+    // Asegurar que la nueva posición no exceda la longitud del valor del textarea
+    const adjustedPosition =
+      newCursorPosition <= textarea.value.length
+        ? newCursorPosition
+        : textarea.value.length;
+    textarea.setSelectionRange(adjustedPosition, adjustedPosition);
+  }
 }
 
--- src/app/testeditor-historia/testeditor-historia.component.ts
+++ src/app/testeditor-historia/testeditor-historia.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, HostListener, Input, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
@@ -12,116 +12,131 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-testeditor-historia',
   standalone: true,
-  imports: [CKEditorModule,FormsModule,ReactiveFormsModule,CommonModule ],
+  imports: [CKEditorModule, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './testeditor-historia.component.html',
-  styleUrl: './testeditor-historia.component.css'
+  styleUrl: './testeditor-historia.component.css',
 })
-export class TesteditorHistoriaComponent implements OnInit,AfterViewInit {
-
-
+export class TesteditorHistoriaComponent implements OnInit, AfterViewInit {
   public Editor = ClassicEditor;
   fullScreenMode = false;
-  @Input() parametro: string= '';
+  @Input() parametro: string = '';
   listahistorias: Historia[];
   historias: Historia[];
-  historiaSeleccionada: number=0;
+  historiaSeleccionada: number = 0;
   his: Historia;
   expediente: Expediente;
 
+  constructor(private Service: Service) {}
 
-
-  constructor(private Service: Service){}
-  
   ngOnInit(): void {
     this.cargarExpedientePaciente(this.parametro);
     this.cargarListaHistoria();
-   this.expediente={
-    clave:parseInt(this.parametro),
-    expediente1:"",
-    id:0,
-    historiaId:""
-  };
+    this.expediente = {
+      clave: parseInt(this.parametro),
+      expediente1: '',
+      id: 0,
+      historiaId: '',
+    };
   }
 
   ngAfterViewInit(): void {
-    this.ajustarAltura(document.getElementById('nombreinputtexthistoria') as HTMLTextAreaElement);
-  
- }
+    this.ajustarAltura(
+      document.getElementById('nombreinputtexthistoria') as HTMLTextAreaElement
+    );
+  }
 
   ajustarAltura(elemento: HTMLTextAreaElement): void {
     elemento.style.height = 'auto'; // Resetea la altura para calcular correctamente
     elemento.style.height = elemento.scrollHeight + 'px'; // Ajusta la altura al contenido
-}
-  cargarListaHistoria(){
-    this.Service.getUnico('GetAllHistorias').subscribe(
-      (data: Historia[]) => {
-        if(data!=null){
-        this.historias=data;
-      }}
-    );
+  }
+  cargarListaHistoria() {
+    this.Service.getUnico('GetAllHistorias').subscribe((data: Historia[]) => {
+      if (data != null) {
+        this.historias = data;
+      }
+    });
   }
 
-  
- guardarHistoriaExpediente() {
-  if(this.historiaSeleccionada==0){
-    return;
-  }else{
-    const histo= this.historias.find(historia => historia.id == this.historiaSeleccionada);
-    if (histo && histo.hc !== null) {
-      this.expediente.expediente1 += histo.hc; // Agregar el contenido de histo.hc al final de this.data
-      this.expediente.historiaId=this.historiaSeleccionada.toString();
-      setTimeout(() => this.ajustarAltura(document.getElementById('nombreinputtexthistoria') as HTMLTextAreaElement), 0);
+  guardarHistoriaExpediente() {
+    if (this.historiaSeleccionada == 0) {
+      return;
+    } else {
+      const histo = this.historias.find(
+        (historia) => historia.id == this.historiaSeleccionada
+      );
+      if (histo && histo.hc !== null) {
+        this.expediente.expediente1 += histo.hc; // Agregar el contenido de histo.hc al final de this.data
+        this.expediente.historiaId = this.historiaSeleccionada.toString();
+        setTimeout(
+          () =>
+            this.ajustarAltura(
+              document.getElementById(
+                'nombreinputtexthistoria'
+              ) as HTMLTextAreaElement
+            ),
+          0
+        );
+      }
     }
   }
-  }
 
-  subcribeExpediente(){
-    
-  }
+  subcribeExpediente() {}
 
-  cargarExpedientePaciente(parametrourl:any){
+  cargarExpedientePaciente(parametrourl: any) {
     this.Service.getUnicoParams('GetExpediente', parametrourl).subscribe(
       (data: Expediente) => {
-        if(data!=null){
-        this.expediente={
-          clave:data.clave,
-          expediente1: data.expediente1 || "",
-          id:data.id,
-          historiaId:data.historiaId
-        }       
+        if (data != null) {
+          this.expediente = {
+            clave: data.clave,
+            expediente1: data.expediente1 || '',
+            id: data.id,
+            historiaId: data.historiaId,
+          };
         }
-      setTimeout(() => this.ajustarAltura(document.getElementById('nombreinputtexthistoria') as HTMLTextAreaElement), 0);
-    }
+        setTimeout(
+          () =>
+            this.ajustarAltura(
+              document.getElementById(
+                'nombreinputtexthistoria'
+              ) as HTMLTextAreaElement
+            ),
+          0
+        );
+      }
     );
- 
   }
 
-  
-
   guardarHistoriadentroExpediente() {
-    let expedienteresult:Expediente;  
-      expedienteresult=this.expediente;
+    let expedienteresult: Expediente;
+    expedienteresult = this.expediente;
 
-      this.Service.postData('PostExpediente',expedienteresult).subscribe(
-        (result)=>{
-            this.expediente={
-              clave:this.expediente.clave,
-              expediente1:result.expediente1 || "",
-              id:result.id,
-              historiaId:result.historiaId
-              
-            }
-            setTimeout(() => this.ajustarAltura(document.getElementById('nombreinputtexthistoria') as HTMLTextAreaElement), 0);
-            Swal.fire({
-              position: 'center',
-              icon: 'success',
-              title: 'Se actualizó la historia del paciente',
-              showConfirmButton: false,
-              timer: 2000,
-            });
-        }
-      )   
-    
+    this.Service.postData('PostExpediente', expedienteresult).subscribe(
+      (result) => {
+        this.expediente = {
+          clave: this.expediente.clave,
+          expediente1: result.expediente1 || '',
+          id: result.id,
+          historiaId: result.historiaId,
+        };
+        setTimeout(
+          () =>
+            this.ajustarAltura(
+              document.getElementById(
+                'nombreinputtexthistoria'
+              ) as HTMLTextAreaElement
+            ),
+          0
+        );
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: 'Se actualizó la historia del paciente',
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      }
+    );
+
     // else{
     //   const expediente= this.historias.find(historia => historia.id == this.historiaSeleccionada);
     //    expediente={
@@ -130,9 +145,69 @@ export class TesteditorHistoriaComponent implements OnInit,AfterViewInit {
 
     //    };
     // }
-      
-   
-    }
+  }
 
-    
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    const textarea = document.getElementById(
+      'nombreinputtexthistoria'
+    ) as HTMLTextAreaElement;
+    if (!textarea) return;
+
+    const cursorPosition = textarea.selectionStart;
+    const lines = textarea.value.substr(0, cursorPosition).split('\n');
+    const currentLineIndex = lines.length - 1;
+    const currentLine = lines[currentLineIndex];
+
+    if (event.key === 'ArrowUp') {
+      event.preventDefault();
+      this.moveCursorUp(textarea, currentLineIndex, cursorPosition, lines);
+    } else if (event.key === 'ArrowDown') {
+      event.preventDefault();
+      this.moveCursorDown(
+        textarea,
+        currentLineIndex,
+        cursorPosition,
+        lines,
+        currentLine.length
+      );
+    }
+  }
+
+  private moveCursorUp(
+    textarea: HTMLTextAreaElement,
+    currentLineIndex: number,
+    cursorPosition: number,
+    lines: string[]
+  ) {
+    if (currentLineIndex === 0) return; // Ya está en la primera línea
+
+    const previousLineLength = lines[currentLineIndex - 1].length;
+    const newCursorPosition =
+      cursorPosition - lines[currentLineIndex].length - 1;
+
+    // Asegurar que la nueva posición no sea negativa
+    const adjustedPosition = newCursorPosition >= 0 ? newCursorPosition : 0;
+    textarea.setSelectionRange(adjustedPosition, adjustedPosition);
+  }
+
+  private moveCursorDown(
+    textarea: HTMLTextAreaElement,
+    currentLineIndex: number,
+    cursorPosition: number,
+    lines: string[],
+    currentLineLength: number
+  ) {
+    const nextLines = textarea.value.substr(cursorPosition).split('\n');
+    if (nextLines.length <= 1) return; // Ya está en la última línea
+
+    const newCursorPosition = cursorPosition + currentLineLength + 1;
+
+    // Asegurar que la nueva posición no exceda la longitud del valor del textarea
+    const adjustedPosition =
+      newCursorPosition <= textarea.value.length
+        ? newCursorPosition
+        : textarea.value.length;
+    textarea.setSelectionRange(adjustedPosition, adjustedPosition);
+  }
 }
